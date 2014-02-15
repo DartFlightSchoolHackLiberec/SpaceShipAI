3c2439fb28dd3453c0e68d1b107495096e2cede2
Tomas Jukin <tomas@jukin.cz>
2014-02-15 10:43:01 +0100

    Added generated rails app

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpaceShipAI::Application.config.secret_key_base = '1744484b5f9c322bdeecff7bf86afc0d8eb990ad4f1168712dbea2d1239a9d4503ba959e89093e4b2bf5fe04ea0f4c69f9ef8309d7802085895a4841e464787e'
---
+++ server/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpaceShipAI::Application.config.session_store :cookie_store, key: '_SpaceShipAI_session'
