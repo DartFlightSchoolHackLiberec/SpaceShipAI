18a63f3d52e50246201eafed910461724cf282ea
David Votrubec <votrubec.d@st-software.com>
2014-02-15 10:39:03 +0100

    added ng controller (not yet woring)

---
+++ web/ng/SpaceShipController.dart
@@ -0,0 +1,63 @@
+@NgController(
+  selector: '[ship-control]',
+  publishAs: 'ctrl')  
+  class SpaceShipController {
+
+  List<Recipe> recipes;
+  SpaceShipController() {
+    recipes = _loadData();
+  }
+
+  Recipe selectedRecipe;
+
+  void selectRecipe(Recipe recipe) {
+    selectedRecipe = recipe;
+  }
+
+  List<Recipe> _loadData() {
+    return [
+      new Recipe('My Appetizer','Appetizers',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 1),
+      new Recipe('My Salad','Salads',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 3),
+      new Recipe('My Soup','Soups',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 4),
+      new Recipe('My Main Dish','Main Dishes',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 2),
+      new Recipe('My Side Dish','Side Dishes',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 3),
+      new Recipe('My Awesome Dessert','Desserts',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 5),
+      new Recipe('My So-So Dessert','Desserts',
+          ["Ingredient 1", "Ingredient 2"],
+          "Some Directions", 3),
+    ];
+  }
+}
+
+class Recipe {
+  String name;
+  String category;
+  List<String> ingredients;
+  String directions;
+  int rating;
+
+  Recipe(this.name, this.category, this.ingredients, this.directions,
+      this.rating);
+}
+
+class SpaceShipModule extends Module {
+  SpaceShipModule() {
+    type(SpaceShipController);
+  }
+}
+
+void main() {
+  ngBootstrap(module: new MyAppModule());
+}
--- web/spaceshipai.dart
+++ web/spaceshipai.dart
@@ -2,17 +2,5 @@ import 'dart:html';
 import 'package:angular/angular.dart';
 
 void main() {
-    
-  querySelector("#sample_text_id")
-    ..text = "Click me!"
-    ..onClick.listen(reverseText);
-}
-
-void reverseText(MouseEvent event) {
-  var text = querySelector("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector("#sample_text_id").text = buffer.toString();
+    ngBootstrap(module: new MyAppModule());
 }
